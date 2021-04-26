5518fa0eda960214710af58f38315d87f5d8504a
fudashuai <fudashuai@bixin.cn>
2021-04-26 19:27:57 +0800

    feat: vap动画

--- app/src/main/java/com/jinxian/wenshi/module_main/adapter/HomeTabListAdapter.kt
+++ app/src/main/java/com/jinxian/wenshi/module_main/adapter/HomeTabListAdapter.kt
@@ -3,6 +3,7 @@ package com.jinxian.wenshi.module_main.adapter
 
 import android.content.res.ColorStateList
 import android.graphics.Color
+import android.os.Environment
 import android.view.View
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.viewholder.BaseDataBindingHolder
@@ -10,11 +11,15 @@ import com.jinxian.wenshi.R
 import com.jinxian.wenshi.databinding.LayoutHomeTabItemBinding
 import com.jinxian.wenshi.module_main.model.TalkModel
 import com.jinxian.wenshi.module_main.viewmodel.HomeViewModel
-import kotlinx.android.synthetic.main.layout_home_tab_item.view.*
+import com.tencent.qgame.animplayer.AnimView
+import com.tencent.qgame.animplayer.util.ScaleType
+import java.io.File
 
 class HomeTabListAdapter(viewModel: HomeViewModel) :
     BaseQuickAdapter<TalkModel, BaseDataBindingHolder<LayoutHomeTabItemBinding>>(R.layout.layout_home_tab_item) {
     val TAG = "ListNormalAdapter22"
+    private val mVideoAnimPath =
+        Environment.getExternalStorageDirectory().absolutePath + "/byteflow/anim_video.mp4"
     val mViewModel = viewModel
 
 
@@ -32,10 +37,19 @@ class HomeTabListAdapter(viewModel: HomeViewModel) :
 
             it.ivLike.setOnClickListener { its ->
                 it.ivLike.imageTintList = ColorStateList.valueOf(Color.RED)
-                it.tvLikeCount.text = item.likeCount ++.toString()
+                it.tvLikeCount.text = item.likeCount++.toString()
+                playVideoAnim(it.videoAnimView)
             }
+            initVideoAnimPlayer(it.videoAnimView)
         }
+    }
 
+    private fun initVideoAnimPlayer(videoAnimView: AnimView) {
+        videoAnimView.setScaleType(ScaleType.FIT_CENTER)
+    }
+
+    private fun playVideoAnim(videoAnimView: AnimView) {
+        videoAnimView.startPlay(File(mVideoAnimPath))
     }
 
 
