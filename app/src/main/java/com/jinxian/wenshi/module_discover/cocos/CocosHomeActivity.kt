3b9edfcbf330324754405fb673657f2ba17fe48d
fudashuai <fudashuai@bixin.cn>
2021-06-18 11:55:30 +0800

    feat:添加cocos游戏

--- app/src/main/java/com/jinxian/wenshi/module_discover/cocos/CocosHomeActivity.kt
+++ app/src/main/java/com/jinxian/wenshi/module_discover/cocos/CocosHomeActivity.kt
@@ -5,6 +5,7 @@ import android.app.DownloadManager
 import android.content.Intent
 import android.os.Environment
 import android.util.Log
+import androidx.appcompat.widget.AppCompatButton
 import com.cocos.lib.GlobalObject
 import com.jinxian.wenshi.R
 import com.jinxian.wenshi.base.activity.BaseActivity
@@ -15,64 +16,79 @@ import kotlinx.android.synthetic.main.activity_cocos_home.*
 
 class CocosHomeActivity : BaseActivity() {
     val download1Url = "http://file.jinxianyun.com/default.zip"
+    val download2Url = "http://file.jinxianyun.com/hellococos.zip"
 
     override fun getLayoutId() = R.layout.activity_cocos_home
 
     override fun initView() {
+        icBack.setOnClickListener {
+            finish()
+        }
+
         DownloadUtil.instance.addListener(listener)
-        btnDownload.setOnClickListener {
+        btnDownload1.setOnClickListener {
             DownloadUtil.instance.download(download1Url)
+        }
+        btnDownload2.setOnClickListener {
+            DownloadUtil.instance.download(download2Url)
         }
     }
 
     val listener: DownloadListener = object : DownloadListener {
         override fun callBack(taskId: Int, status: Int, progress: Int) {
-            Log.i("nell--", "taskId:$taskId, status:$status, progress:$progress")
-            if (DownloadUtil.instance.taskMap.get(download1Url) == taskId) {
-                when (status) {
-                    DownloadManager.STATUS_RUNNING -> {
-                        btnDownload?.text = "下载中${progress}%"
-                    }
-                    DownloadManager.STATUS_SUCCESSFUL -> {
-                        btnDownload?.text = "解压中"
-                        val zipFileString =
-                            "${getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath}/${
-                                download1Url.split("/").last()
-                            }"
-                        val outPathString =
-                            getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
-                        FileUtil.unzip(
-                            zipFileString,
-                            outPathString,
-                            object : FileUtil.ZipProgress {
-                                override fun onProgress(progress: Int) {
-                                    btnDownload?.text = "解压中$progress"
-                                }
+            Log.i("nell--", "taskId:$taskId, status:$status, progress:$progress, url1taskId:${DownloadUtil.instance.taskMap.get(download1Url)}")
 
-                                override fun onDone() {
-                                    btnDownload?.text = "打开游戏"
-                                    btnDownload?.setOnClickListener {
-                                        val intent = Intent(
-                                            this@CocosHomeActivity,
-                                            CocosGameActivity::class.java
-                                        )
-                                        intent.putExtra(
-                                            "path",
-                                            "${outPathString}/${
-                                                download1Url.split("/").last().split(".").first()
-                                            }"
-                                        )
-                                        startActivity(intent)
-                                    }
+            updateUI(download1Url, btnDownload1, taskId, status, progress)
+            updateUI(download2Url, btnDownload2, taskId, status, progress)
+        }
+    }
+
+    fun updateUI(url: String, btn: AppCompatButton, taskId: Int, status: Int, progress: Int) {
+        if (DownloadUtil.instance.taskMap.get(url) == taskId) {
+            when (status) {
+                DownloadManager.STATUS_RUNNING -> {
+                    btn?.text = "下载中${progress}%"
+                }
+                DownloadManager.STATUS_SUCCESSFUL -> {
+                    btn?.text = "解压中"
+                    val zipFileString =
+                        "${getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath}/${
+                            url.split("/").last()
+                        }"
+                    val outPathString =
+                        getExternalFilesDir(Environment.DIRECTORY_DOWNLOADS)?.absolutePath
+                    FileUtil.unzip(
+                        zipFileString,
+                        outPathString,
+                        object : FileUtil.ZipProgress {
+                            override fun onProgress(progress: Int) {
+                                btn?.text = "解压中$progress"
+                            }
+
+                            override fun onDone() {
+                                btn?.text = "打开游戏"
+                                btn?.setOnClickListener {
+                                    val intent = Intent(
+                                        this@CocosHomeActivity,
+                                        CocosGameActivity::class.java
+                                    )
+                                    intent.putExtra(
+                                        "path",
+                                        "${outPathString}/${
+                                            url.split("/").last().split(".").first()
+                                        }"
+                                    )
+                                    startActivity(intent)
                                 }
                             }
-                        )
-                    }
-                    DownloadManager.STATUS_FAILED -> {
-                        btnDownload?.text = "下载失败"
-                    }
+                        }
+                    )
+                }
+                DownloadManager.STATUS_FAILED -> {
+                    btn?.text = "下载失败"
                 }
             }
         }
+
     }
 }
